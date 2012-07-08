267dd28c380e8eb73d2efc6a9df6b3c1f3e876e6
Thomas Gorissen <mail@serrynaimo.com>
2012-07-08 21:59:11 +0800

    Little bug fixing ;)

--- static1/js/main/00-jscamp.js
+++ static1/js/main/00-jscamp.js
@@ -131,9 +131,11 @@ $("#pins li").hover(function() {
 })
 
 
-function routeTo(href) {	
+function routeTo(href) {
 	var goto = (href.indexOf("http") == 0) ? href.substr(19).toLowerCase() : href;
 	var offset = 110;
+	if($("#" + goto).length)
+		$("#home").css({height: "680px"});
 	if(goto == "talks" || goto == "sprints" || goto == "workshops") {
 		$("#"+goto).click();
 	}
