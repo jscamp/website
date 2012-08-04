5a88c3e39026ebdb0f7f769b07d1d63b0e0c7501
Thomas Gorissen <mail@serrynaimo.com>
2012-08-04 18:07:00 +0800

    Release for Paypal and ticket prices

--- static3/js/main/00-jscamp.js
+++ static3/js/main/00-jscamp.js
@@ -133,7 +133,7 @@ $("#pins li").hover(function() {
 
 function routeTo(href) {
 	var goto = (href.indexOf("http") == 0) ? href.substr(19).toLowerCase() : href;
-	var offset = 110;
+	var offset = goto == "sponsors" || goto == "info" ? 0 : 110;
 	if($("#" + goto).length)
 		$("#home").css({height: "680px"});
 	if(goto == "talks" || goto == "sprints" || goto == "workshops") {
