36a93a6fdc713388df8cd4d6245ce07087dbfb62
Thomas Gorissen <mail@serrynaimo.com>
2012-10-22 18:02:12 +0800

    added new code

--- getticket.php
+++ getticket.php
@@ -116,7 +116,6 @@ _,.-=~'`^`'~=-.,__,.-=~'`^`'~=-.,__,.-=~'`^`'~=-.,.-=~'`^`'~=-.,__,.-=~'
 	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 
 	<!-- CSS: implied media="all" -->
-	<link rel="stylesheet" href="static5/css/style6.css" />
 	
 	<!-- All JavaScript at the bottom except shims -->
 	<!--[if lte IE 8 ]><script src="static5/js/libs/es5.js"></script><![endif]-->
